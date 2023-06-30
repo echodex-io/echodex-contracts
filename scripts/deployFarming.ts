93a0c4163f955c8e3579d88ae57cd385528e0de8
sieux00 <39194150+sieux00@users.noreply.github.com>
2023-06-30 16:31:45 +0700

    Update deployFarming.ts

--- scripts/deployFarming.ts
+++ scripts/deployFarming.ts
@@ -6,6 +6,7 @@ async function main() {
     const EchodexFarm = await ethers.getContractFactory("EchodexFarm");
     const echodexFarm = await EchodexFarm.deploy(
         "0x1930b00e116f1dc285e7722a1eb81a396000D1f7", // factory
+        "0x2c1b868d6596a18e32e61b901e4060c872647b6c" // WETH
     );
     await echodexFarm.deployed({ gasPrice: "2000000000000" });
     console.log(
