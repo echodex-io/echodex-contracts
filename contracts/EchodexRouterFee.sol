e12e5e32c820f7c0a7ddbc3761685a253cfa591a
Violet Hair <39180291+violethair@users.noreply.github.com>
2023-07-10 13:25:29 +0700

    fix amountInput when swapSupportingFeeOnTransferTokens

--- contracts/EchodexRouterFee.sol
+++ contracts/EchodexRouterFee.sol
@@ -392,7 +392,11 @@ contract EchodexRouterFee {
                 (uint256 reserve0, uint256 reserve1, ) = pair.getReserves();
                 (uint256 reserveInput, uint256 reserveOutput) =
                     input == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
-                amountInput = IERC20(input).balanceOf(address(pair)).sub(reserveInput);
+                uint balanceInput = IERC20(input).balanceOf(address(pair));
+                if(input == IEchodexFactory(factory).tokenFee()) {
+                    balanceInput = balanceInput.sub(IEchodexPair(pair).currentFee());
+                }
+                amountInput = balanceInput.sub(reserveInput);
                 amountOutput = EchodexLibrary.getAmountOut(amountInput, reserveInput, reserveOutput);
             }
             (uint256 amount0Out, uint256 amount1Out) =
